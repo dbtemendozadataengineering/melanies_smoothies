d4e74f47955cdf698a26382f9dcaf88a8a75db9d
Eduardo Mendoza <eduardo.mendoza@civica-soft.com>
2024-07-05 13:00:19 +0200

    Update streamlit_app.py
    Add API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,10 @@ if ingredients_list:
     #st.stop
     time_to_insert = st.button('Submit Order')
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
